3524d2e9c4dda18b56d23474c922b734d047a2dc
GoHoo <potoo10@yahoo.com>
2022-07-01 23:41:29 +0300

    Create NaN.py

---
+++ 3 Simple Functions/NaN.py
@@ -0,0 +1,15 @@
+x = 1.56
+print( int( x ) )
+print( x )
+##############
+
+x = 2.1
+y = ' 13 '
+z = int( x )
+print( z )
+print( int( y ) )
+###################
+
+base = 2
+exponent = 3
+print( pow( base, exponent ) )
